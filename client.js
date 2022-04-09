44060381ada58ab7c4c14620444ff7a1fde93758
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-04-09 11:27:15 -0500

    Employees are now being added to the DOM

--- client.js
+++ client.js
@@ -1,16 +1,23 @@
 $(document).ready(onReady);
 
-function onReady() {
-    $('#submitButton').on('click', employeeInfo)
+function onReady(){
+    $('#submitButton').on('click', employeeAppender);
 }
 
-function employeeInfo() {
-    // console.log('in employeeInfo');
-    $('#employeeDetails').append(`<tr>
-    <td>` + $('#firstName').val() + `</td>
-    <td>` + $('#lastName').val() + `</td>
-    <td>` + $('#idNumber').val() + `</td>
-    <td>` + $('#jobTitle').val() + `/td>
-    <td>` + $('#annualSalary').val() + `</td>
-    </tr>`)
+// let newEmployee = {
+//     firstName: $('#firstName').val(),
+//     lastName: $('#lastName').val(),
+//     idNumber: $('#idNumber').val(),
+//     jobTitle: $('#jobTitle').val(),
+//     annualSalary: $('#annualSalary').val()
+// }
+
+function employeeAppender(){
+    $('.employeeDetails').append(`<tr>
+    <td>${$('#firstName').val()}</td>
+    <td>${$('#lastName').val()}</td>
+    <td>${$('#idNumber').val()}</td>
+    <td>${$('#jobTitle').val()}</td>
+    <td>${$('#annualSalary').val()}</td>
+    </tr>`);
 }
