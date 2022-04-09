5c121ceb6bd8783a26fc2f851fe83559e1de7065
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-04-09 18:26:50 -0500

    the monthly salary is now correctly updating when an employee is deleted

--- client.js
+++ client.js
@@ -3,9 +3,17 @@ $(document).ready(onReady);
 function onReady(){
     $('#submitButton').on('click', employeeAppender);
     $('#submitButton').on('click', monthlySalaryCalculator);
+    $(document).on('click', '.delete', deleteEmployee);
 }
 
 let monthlySalary = 0;
+let newEmployee = {
+    firstName: $('#firstName').val(),
+    lastName: $('#lastName').val(),
+    idNumber: $('#idNumber').val(),
+    jobTitle: $('#jobTitle').val(),
+    annualSalary: $('#annualSalary').val()
+}
 
 function employeeAppender(){
     $('.employeeDetails').append(`<tr>
@@ -13,12 +21,12 @@ function employeeAppender(){
     <td>${$('#lastName').val()}</td>
     <td>${$('#idNumber').val()}</td>
     <td>${$('#jobTitle').val()}</td>
-    <td>${$('#annualSalary').val()}</td>
-    <td><button class="delete">Delete</button></td>
+    <td class="salary">${$('#annualSalary').val()}</td>
+    <td><button class="delete" style="background-color: rgb(239, 239, 239)",
+    "color: black">Delete</button></td>
     </tr>`);
     $('.delete').css({"margin": "auto", "display": "block"});
-    $('.delete').on('click', deleteEmployee);
-    monthlySalary += parseInt($('#annualSalary').val() / 12);
+    monthlySalary += $('#annualSalary').val() / 12;
     $('.inputs').val('');
 }
 
@@ -27,9 +35,21 @@ function monthlySalaryCalculator(){;
     $('#salaryTotal').append(monthlySalary.toFixed(2));
     if (monthlySalary >= 20000) {
         $('footer').css({"background-color": "black", "color": "red"});
+        $('#bottomLine').css({"background-color": "black", "color": "red"});
+        $('.delete').css({"background-color": "red", "color": "white"});
     }
 }
 
 function deleteEmployee() {
+    let negativeSalary = $(this).closest('tr').children('td.salary').text();
     $(this).closest("tr").remove();
+    monthlySalary -= (negativeSalary / 12);
+    $('#salaryTotal').empty();
+    $('#salaryTotal').append(monthlySalary.toFixed(2));
+    if (monthlySalary < 20000) {
+        $('footer').css({"background-color": "salmon", "color": "gray"});
+        $('#bottomLine').css({"background-color": "gray", "color": "white"});
+        $('.delete').css({"background-color": "rgb(239, 239, 239)",
+        "color": "black"});
+    }
 }
